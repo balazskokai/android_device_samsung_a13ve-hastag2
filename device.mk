7e946bf01aa315363e2ea593f806dde37286e4a3
GitFASTBOOT <ahmedshahinx0x@gmail.com>
2025-01-03 20:12:02 +0200

    a13ve: build fastboot package

--- device.mk
+++ device.mk
@@ -18,3 +18,8 @@ LOCAL_PATH := device/samsung/a13ve
 
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
